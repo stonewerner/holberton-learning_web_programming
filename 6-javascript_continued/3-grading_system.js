49982350ad023fc27a357dcf726b756f3377e627
stonewerner <stonewerner@gmail.com>
2019-07-15 10:49:01 -0700

    Create 3-grading_system.js

---
+++ 6-javascript_continued/3-grading_system.js
@@ -0,0 +1,24 @@
+//JAVASCRIPT
+var x = process.argv[2];
+if (x >= 95){
+  console.log("You received an A+");
+}
+else if (x>= 90){
+  console.log("You received an A");
+}
+else if (x>= 85){
+  console.log("You received a B+");
+}
+else if (x>= 80){
+  console.log("You received a B");
+}
+else if (x>= 75){
+  console.log("You received a C+");
+}
+else if (x>= 70){
+  console.log("You received a C");
+}
+else if (x<= 65){
+  console.log("You received a D");
+}
+//NOT HOW A REAL GRADING SYSTEM WORKS: WHAT IF YOU GET A 69???????
