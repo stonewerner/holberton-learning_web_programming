4c00e98470d34febe9d6059ded59297a66b89b82
stonewerner <stonewerner@gmail.com>
2019-07-15 10:39:34 -0700

    Create 1-condition.js

---
+++ 6-javascript_continued/1-condition.js
@@ -0,0 +1,6 @@
+//JAVASCRIPT
+var my_parameter = process.argv[2];
+if (my_parameter > 5){
+  console.log("The value is greater than 5");
+}
+//if it is less than or equal to we come down here and nothing is printed
