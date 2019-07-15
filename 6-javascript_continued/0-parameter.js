4d690a5b51b026a862186f11c77f5c43d6efa56f
stonewerner <stonewerner@gmail.com>
2019-07-15 10:24:10 -0700

    Update 0-parameter.js

--- 6-javascript_continued/0-parameter.js
+++ 6-javascript_continued/0-parameter.js
@@ -1,3 +1,12 @@
 //JAVASCRIPT
 var my_parameter = process.argv[2];
-console.log("Here is the first passed parameter:" my_parameter);
+console.log("Here is the first passed parameter:" + my_parameter);
+
+
+//## Run `sudo apt-get install -y nodejs` to install Node.js 12.x and npm
+//## You may also need development tools to build native addons:
+//     sudo apt-get install gcc g++ make
+//## To install the Yarn package manager, run:
+//     curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+//     echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+//     sudo apt-get update && sudo apt-get install yarn
